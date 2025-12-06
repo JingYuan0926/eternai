f7e9d3c160b13ef47b3daa5712eb91cb6f374e1e
Cedric <cedricctf11a@gmail.com>
2025-12-06 18:11:06 +0800

    added docstring in menu overlay component

--- components/MenuOverlay.js
+++ components/MenuOverlay.js
@@ -3,6 +3,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import ContourBackground from './ContourBackground';
 
+/**
+ * A full-screen overlay menu component with animated transitions and image previews.
+ * Displays navigation links and interactive visual elements.
+ *
+ * @param {Object} props
+ * @param {boolean} props.isOpen - Whether the menu overlay is currently open.
+ * @param {Function} props.onClose - Callback function to close the menu.
+ * @param {string} [props.defaultActiveItem='HOME'] - The label of the menu item to be active by default.
+ */
 export default function MenuOverlay({ isOpen, onClose, defaultActiveItem = 'HOME' }) {
     const menuItems = [
         { label: 'HOME', href: '/landing' },
